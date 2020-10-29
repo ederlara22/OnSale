4f296d6aa56c658ca8440176a1d70f8ec822923c
inteco2207@gmail.com <inteco2207@gmail.com>
2020-10-28 22:27:40 -0500

    eder inicios

---
+++ OnSale.common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace OnSale.common
+{
+    public class Class1
+    {
+    }
+}
---
+++ OnSale.common/Entities/Country.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace OnSale.common.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        [MaxLength(50)]
+        [Required]
+        public string Name { get; set; }
+    }
+}
